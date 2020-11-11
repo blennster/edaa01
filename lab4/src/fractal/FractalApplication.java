3dc842fd880acd6c7b1627c78062aeef2d3e3343
Emil Blennow <emil.blennow99@gmail.com>
2020-11-11 10:56:25 +0100

    Gjorde sne

--- lab4/src/fractal/FractalApplication.java
+++ lab4/src/fractal/FractalApplication.java
@@ -5,11 +5,14 @@ import mountain.Mountain;
 import mountain.Point;
 
 public class FractalApplication {
-	public static void main(String[] args) {
-		Fractal[] fractals = new Fractal[2];
-		fractals[0] = new Mountain(new Point(100, 500), new Point(300, 200), new Point(500, 500), 50);
-		fractals[1] = new Koch(300);
-	    new FractalView(fractals, "Fraktaler", 600, 600);
-	}
-
+    public static void main(String[] args) {
+        Fractal[] fractals = new Fractal[2];
+        fractals[0] = new Mountain(
+                new Point(50, 400),
+                new Point(400, 200),
+                new Point(500, 570),
+                50);
+        fractals[1] = new Koch(300);
+        new FractalView(fractals, "Fraktaler", 600, 600);
+    }
 }
